88e7650f3c8505189172682e509d42a5664ae157
João Vítor Gonçalves <jvgoncalves935@hotmail.com>
2025-12-18 05:45:38 -0300

    Petit Dragon

--- MDPro3/script/c69069007.lua
+++ MDPro3/script/c69069007.lua
@@ -1,11 +1,9 @@
 --Petit Dragon fodase kkkkkkk
 local s,id,o=GetID()
 function s.initial_effect(c)
-	---------------------------------------------
 	-- Continuous ATK boost
 	-- If you have 20+ "Universo G" cards in your Deck,
 	-- Dragon monsters you control gain 6000 ATK
-	---------------------------------------------
 	local e1=Effect.CreateEffect(c)
 	e1:SetType(EFFECT_TYPE_FIELD)
 	e1:SetCode(EFFECT_UPDATE_ATTACK)
@@ -15,6 +13,19 @@ function s.initial_effect(c)
 	e1:SetCondition(s.atkcon)
 	e1:SetValue(6000)
 	c:RegisterEffect(e1)
+	
+	-- Continuous ATK boost
+	-- If you have 20+ "Universo G" cards in your Deck,
+	-- Dragon monsters you control gain 6000 DEF
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD)
+	e2:SetCode(EFFECT_UPDATE_DEFENSE)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetTargetRange(LOCATION_MZONE,0)
+	e2:SetTarget(s.atktg)
+	e2:SetCondition(s.atkcon)
+	e2:SetValue(6000)
+	c:RegisterEffect(e2)
 end
 
 function s.atkcon(e)
