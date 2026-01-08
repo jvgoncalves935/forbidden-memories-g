5f5672a2482feda9547272fa480a7d904bd42e20
João Vítor Gonçalves <jvgoncalves935@hotmail.com>
2026-01-08 18:13:44 -0300

    Mangueira Evil

--- MDPro3/script/c69115019.lua
+++ MDPro3/script/c69115019.lua
@@ -43,6 +43,20 @@ function s.initial_effect(c)
 	local e5=e3:Clone()
 	e5:SetCode(EVENT_FLIP_SUMMON_SUCCESS)
 	c:RegisterEffect(e5)
+
+	-- Quick Effect: Special Summon + Xyz Summon
+	local e6=Effect.CreateEffect(c)
+	e6:SetDescription(aux.Stringid(id,3))
+	e6:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e6:SetType(EFFECT_TYPE_QUICK_O)
+	e6:SetCode(EVENT_FREE_CHAIN)
+	e6:SetRange(LOCATION_MZONE)
+	e6:SetHintTiming(0,TIMINGS_CHECK_MONSTER+TIMING_END_PHASE)
+	e6:SetCountLimit(1,id+300) -- hard OPT
+	e6:SetTarget(s.xyztg)
+	e6:SetOperation(s.xyzop)
+	c:RegisterEffect(e6)
+
 end
 
 function s.spcon_proc(e,c)
@@ -87,3 +101,99 @@ function s.lvop(e,tp,eg,ep,ev,re,r,rp)
 	end
 end
 
+function s.deckfilter_tg(c,lv)
+	return c:IsSetCard(0xc50)
+		and c:IsLevel(lv)
+end
+
+
+function s.basefilter(c,tp)
+	local lv=c:GetLevel()
+	return c:IsFaceup()
+		and c:IsSetCard(0xc50)
+		and lv<=6
+		and Duel.IsExistingMatchingCard(s.deckfilter_tg,tp,LOCATION_DECK,0,1,nil,lv)
+		and Duel.IsExistingMatchingCard(s.xyzfilter,tp,LOCATION_EXTRA,0,1,nil,lv)
+end
+
+function s.deckfilter(c,lv,e,tp)
+	return c:IsSetCard(0xc50)
+		and c:IsLevel(lv)
+		and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+
+function s.matfilter(c,lv)
+	return c:IsFaceup()
+		and c:IsSetCard(0xc50)
+		and c:IsLevel(lv)
+end
+
+function s.xyzfilter(c,lv)
+	return c:IsSetCard(0xc50)
+		and c:IsType(TYPE_XYZ)
+		and c:GetRank()==lv
+		and c:GetRank()<=6
+end
+
+function s.xyztg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then
+		if Duel.GetLocationCount(tp,LOCATION_MZONE)<=0 then return false end
+		return Duel.IsExistingMatchingCard(s.basefilter,tp,LOCATION_MZONE,0,1,nil,tp)
+	end
+end
+
+function s.xyzop(e,tp,eg,ep,ev,re,r,rp)
+	-- Seleciona o monstro base CORRETO
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_FACEUP)
+	local base=Duel.SelectMatchingCard(tp,s.basefilter,tp,LOCATION_MZONE,0,1,1,nil,tp):GetFirst()
+	if not base then return end
+	local lv=base:GetLevel()
+
+	-- Invoca do Deck
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectMatchingCard(tp,s.deckfilter,tp,LOCATION_DECK,0,1,1,nil,lv,e,tp)
+	local sc=g:GetFirst()
+	if not sc then return end
+	if Duel.SpecialSummon(sc,0,tp,tp,false,false,POS_FACEUP)==0 then return end
+
+	-- Nega efeitos do monstro invocado
+	local e1=Effect.CreateEffect(e:GetHandler())
+	e1:SetType(EFFECT_TYPE_SINGLE)
+	e1:SetCode(EFFECT_DISABLE)
+	e1:SetReset(RESET_EVENT+RESETS_STANDARD)
+	sc:RegisterEffect(e1)
+
+	local e2=e1:Clone()
+	e2:SetCode(EFFECT_DISABLE_EFFECT)
+	sc:RegisterEffect(e2)
+
+	-- Grupo EXATO de matérias
+	local mg=Group.CreateGroup()
+	mg:AddCard(base)
+	mg:AddCard(sc)
+
+	-- Seleciona o Xyz válido
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local xyz=Duel.SelectMatchingCard(tp,s.xyzfilter,tp,LOCATION_EXTRA,0,1,1,nil,lv):GetFirst()
+	if not xyz then return end
+
+	-- Invocação-Xyz manual CORRETA
+	xyz:SetMaterial(mg)
+	Duel.Overlay(xyz,mg)
+
+	if Duel.SpecialSummon(xyz,SUMMON_TYPE_XYZ,tp,tp,false,false,POS_FACEUP)>0 then
+		xyz:CompleteProcedure()
+	end
+
+	-- Lock de Xyz
+	local e3=Effect.CreateEffect(e:GetHandler())
+	e3:SetType(EFFECT_TYPE_FIELD)
+	e3:SetProperty(EFFECT_FLAG_PLAYER_TARGET+EFFECT_FLAG_OATH)
+	e3:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
+	e3:SetTargetRange(1,0)
+	e3:SetTarget(function(e,c)
+		return c:IsType(TYPE_XYZ)
+	end)
+	e3:SetReset(RESET_PHASE+PHASE_END)
+	Duel.RegisterEffect(e3,tp)
+end
