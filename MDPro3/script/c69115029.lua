196de1afd25428818427b64dff7cdd52e5d011a0
João Vítor Gonçalves <jvgoncalves935@hotmail.com>
2026-02-11 21:51:15 -0300

    Fix Paulo Gino Piscineiro e Bombeiro

--- MDPro3/script/c69115029.lua
+++ MDPro3/script/c69115029.lua
@@ -1,5 +1,122 @@
 --Paulo Gino Piscineiro
-local s,id,o=GetID()
+local s,id=GetID()
 function s.initial_effect(c)
+	c:EnableReviveLimit()
+
+	-- Link Summon
+	aux.AddLinkProcedure(c,s.matfilter,1,1)
 	
+	-- 1) Link Summon → revive Link "Universo G" (HOPT)
+	local e1=Effect.CreateEffect(c)
+	e1:SetDescription(aux.Stringid(id,0))
+	e1:SetProperty(EFFECT_FLAG_CARD_TARGET)
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e1:SetCondition(s.spcon1)
+	e1:SetTarget(s.sptg1)
+	e1:SetOperation(s.spop1)
+	e1:SetCountLimit(1,id)
+	c:RegisterEffect(e1)
+
+	-- 2) Contínuo obrigatório: se o oponente adicionar/comprar no seu turno → você compra
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e2:SetCode(EVENT_DRAW)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCondition(s.drcon)
+	e2:SetOperation(s.drop)
+	e2:SetCountLimit(1,id+200)
+	c:RegisterEffect(e2)
+
+	-- 3) Quick: pagar 1500 + tributar → recuperar Extra (HOPT)
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,1))
+	e3:SetCategory(CATEGORY_TOHAND)
+	e3:SetType(EFFECT_TYPE_QUICK_O)
+	e3:SetCode(EVENT_FREE_CHAIN)
+	e3:SetRange(LOCATION_MZONE)
+	e3:SetCountLimit(1,id+400)
+	e3:SetCost(s.thcost)
+	e3:SetTarget(s.thtg)
+	e3:SetOperation(s.thop)
+	c:RegisterEffect(e3)
 end
+
+-- Material Link
+function s.matfilter(c,lc,sumtype,tp)
+	return c:IsSetCard(0xc50)
+end
+
+-- 1) Link Summon revive
+function s.spcon1(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():IsSummonType(SUMMON_TYPE_LINK)
+end
+
+function s.spfilter1(c,e,tp)
+	return c:IsSetCard(0xc50)
+		and c:IsType(TYPE_LINK)
+		and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+
+function s.sptg1(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then
+		return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+			and Duel.IsExistingTarget(s.spfilter1,tp,LOCATION_GRAVE,0,1,nil,e,tp)
+	end
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+	local g=Duel.SelectTarget(tp,s.spfilter1,tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,0,0)
+end
+
+function s.spop1(e,tp,eg,ep,ev,re,r,rp)
+	local tc=Duel.GetFirstTarget()
+	if tc and tc:IsRelateToEffect(e) then
+		Duel.SpecialSummon(tc,0,tp,tp,false,false,POS_FACEUP)
+	end
+end
+
+-- 2) Oponente comprou/adicionou no seu turno → você compra
+function s.drcon(e,tp,eg,ep,ev,re,r,rp)
+	return ep==1-tp				 -- oponente comprou
+		and Duel.GetTurnPlayer()==tp -- é seu turno
+end
+
+function s.drop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Draw(tp,1,REASON_EFFECT)
+end
+
+-- 3) Quick: pagar 1500 + tributar → recuperar Extra
+function s.thcost(e,tp,eg,ep,ev,re,r,rp,chk)
+	local c=e:GetHandler()
+	if chk==0 then
+		return Duel.CheckLPCost(tp,1500)
+			and c:IsReleasable()
+	end
+	Duel.PayLPCost(tp,1500)
+	Duel.Release(c,REASON_COST)
+end
+
+function s.thfilter(c)
+	return c:IsSetCard(0xc50)
+		and c:IsFaceup()
+		and c:IsAbleToHand()
+end
+
+function s.thtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then
+		return Duel.IsExistingMatchingCard(s.thfilter,tp,LOCATION_EXTRA,0,1,nil)
+	end
+	Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_EXTRA)
+end
+
+function s.thop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+	local g=Duel.SelectMatchingCard(tp,s.thfilter,tp,LOCATION_EXTRA,0,1,1,nil)
+	local tc=g:GetFirst()
+	if tc then
+		Duel.SendtoHand(tc,nil,REASON_EFFECT)
+		Duel.ConfirmCards(1-tp,tc)
+	end
+end
+
--- MDPro3/script/c69115031.lua
+++ MDPro3/script/c69115031.lua
@@ -1,45 +1,77 @@
 --Paulo Gino Bombeiro
---c69115031
 local s,id=GetID()
 function s.initial_effect(c)
-	--Requerimentos de Invocação-Link
 	c:EnableReviveLimit()
-	aux.AddLinkProcedure(c,aux.FilterBoolFunction(Card.IsLinkType,TYPE_EFFECT),2,2)
 
-	--Efeito: Invocação-Especial de 1 monstro "Universo G" do Cemitério
+	-- Link Summon: 1 Link + 1 "Universo G" (exatamente 2)
+	aux.AddLinkProcedure(c,s.lmatfilter,2,2)
+
+	-- 1) Contínuo obrigatório: se o oponente comprar/adicionar no seu turno → você compra (HOPT)
 	local e1=Effect.CreateEffect(c)
-	e1:SetDescription(aux.Stringid(id,0))
-	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
-	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
-	e1:SetProperty(EFFECT_FLAG_DELAY+EFFECT_FLAG_CARD_TARGET)
-	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
-	e1:SetCondition(s.spcon)
-	e1:SetTarget(s.sptg)
-	e1:SetOperation(s.spop)
+	e1:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e1:SetCode(EVENT_DRAW)
+	e1:SetRange(LOCATION_MZONE)
+	e1:SetCondition(s.drcon)
+	e1:SetOperation(s.drop)
+	e1:SetCountLimit(1,id)
 	c:RegisterEffect(e1)
+
+	-- 2) Ignition: revive Link OU "Universo G" do GY (HOPT)
+	local e2=Effect.CreateEffect(c)
+	e2:SetDescription(aux.Stringid(id,0))
+	e2:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e2:SetType(EFFECT_TYPE_IGNITION)
+	e2:SetRange(LOCATION_MZONE)
+	e2:SetCountLimit(1,id+200)
+	e2:SetTarget(s.sptg)
+	e2:SetOperation(s.spop)
+	c:RegisterEffect(e2)
+
+	-- 3) Se enviado do Extra Deck para o GY → escolher efeito (HOPT)
+	local e3=Effect.CreateEffect(c)
+	e3:SetDescription(aux.Stringid(id,1))
+	e3:SetCategory(CATEGORY_TOHAND+CATEGORY_TODECK)
+	e3:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e3:SetCode(EVENT_TO_GRAVE)
+	e3:SetProperty(EFFECT_FLAG_DELAY)
+	e3:SetCountLimit(1,id+400)
+	e3:SetCondition(s.tgcon)
+	e3:SetTarget(s.tgtg)
+	e3:SetOperation(s.tgop)
+	c:RegisterEffect(e3)
 end
 
---Condição: apenas se for Invocado por Invocação-Link
-function s.spcon(e,tp,eg,ep,ev,re,r,rp)
-	return e:GetHandler():IsSummonType(SUMMON_TYPE_LINK)
+-- Link Material
+function s.lmatfilter(c,lc,sumtype,tp)
+	return c:IsType(TYPE_LINK) or c:IsSetCard(0xc50)
 end
 
---Filtro: monstro "Universo G" no Cemitério
+-- 1) Compra contínua
+function s.drcon(e,tp,eg,ep,ev,re,r,rp)
+	return ep==1-tp
+		and Duel.GetTurnPlayer()==tp
+end
+
+function s.drop(e,tp,eg,ep,ev,re,r,rp)
+	Duel.Draw(tp,1,REASON_EFFECT)
+end
+
+-- 2) Ignition revive
 function s.spfilter(c,e,tp)
-	return c:IsSetCard(0xc50) and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+	return (c:IsType(TYPE_LINK) or c:IsSetCard(0xc50))
+		and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
 end
 
---Alvo
-function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk,chkc)
-	if chkc then return chkc:IsControler(tp) and chkc:IsLocation(LOCATION_GRAVE) and s.spfilter(chkc,e,tp) end
-	if chk==0 then return Duel.IsExistingTarget(s.spfilter,tp,LOCATION_GRAVE,0,1,nil,e,tp)
-		and Duel.GetLocationCount(tp,LOCATION_MZONE)>0 end
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then
+		return Duel.GetLocationCount(tp,LOCATION_MZONE)>0
+			and Duel.IsExistingTarget(s.spfilter,tp,LOCATION_GRAVE,0,1,nil,e,tp)
+	end
 	Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
 	local g=Duel.SelectTarget(tp,s.spfilter,tp,LOCATION_GRAVE,0,1,1,nil,e,tp)
 	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,g,1,0,0)
 end
 
---Operação: Invoca o alvo por Invocação-Especial no seu lado do campo
 function s.spop(e,tp,eg,ep,ev,re,r,rp)
 	local tc=Duel.GetFirstTarget()
 	if tc and tc:IsRelateToEffect(e) then
@@ -47,3 +79,66 @@ function s.spop(e,tp,eg,ep,ev,re,r,rp)
 	end
 end
 
+-- 3) Enviado do Extra Deck para o GY
+function s.tgcon(e,tp,eg,ep,ev,re,r,rp)
+	local c=e:GetHandler()
+	return c:IsPreviousLocation(LOCATION_EXTRA)
+end
+
+function s.exfilter(c)
+	return c:IsFaceup()
+		and c:IsAbleToHand()
+end
+
+function s.gyfilter(c)
+	return c:IsAbleToDeck()
+end
+
+function s.tgtg(e,tp,eg,ep,ev,re,r,rp,chk)
+	local b1=Duel.IsExistingMatchingCard(s.exfilter,tp,LOCATION_EXTRA,0,1,nil)
+	local b2=Duel.IsExistingMatchingCard(s.gyfilter,tp,LOCATION_GRAVE,0,1,nil)
+	if chk==0 then return b1 or b2 end
+
+	if b1 then
+		Duel.SetOperationInfo(0,CATEGORY_TOHAND,nil,1,tp,LOCATION_EXTRA)
+	end
+	if b2 then
+		Duel.SetOperationInfo(0,CATEGORY_TODECK,nil,1,tp,LOCATION_GRAVE)
+	end
+end
+
+function s.tgop(e,tp,eg,ep,ev,re,r,rp)
+	local b1=Duel.IsExistingMatchingCard(s.exfilter,tp,LOCATION_EXTRA,0,1,nil)
+	local b2=Duel.IsExistingMatchingCard(s.gyfilter,tp,LOCATION_GRAVE,0,1,nil)
+
+	if not (b1 or b2) then return end
+
+	local op=0
+	if b1 and b2 then
+		op=Duel.SelectOption(tp,
+			aux.Stringid(id,3), -- adicionar do Extra
+			aux.Stringid(id,4)  -- retornar do GY
+		)
+	elseif b1 then
+		op=0
+	else
+		op=1
+	end
+
+	if op==0 then
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_ATOHAND)
+		local g=Duel.SelectMatchingCard(tp,s.exfilter,tp,LOCATION_EXTRA,0,1,1,nil)
+		local tc=g:GetFirst()
+		if tc then
+			Duel.SendtoHand(tc,nil,REASON_EFFECT)
+			Duel.ConfirmCards(1-tp,tc)
+		end
+	else
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_TODECK)
+		local g=Duel.SelectMatchingCard(tp,s.gyfilter,tp,LOCATION_GRAVE,0,1,1,nil)
+		local tc=g:GetFirst()
+		if tc then
+			Duel.SendtoDeck(tc,nil,SEQ_DECKSHUFFLE,REASON_EFFECT)
+		end
+	end
+end
