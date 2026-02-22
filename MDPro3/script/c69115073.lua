9a14179c65342444764f5991ca50e6eae606af5d
João Vítor Gonçalves <jvgoncalves935@hotmail.com>
2026-02-21 22:31:37 -0300

    Rock Gaucho

--- MDPro3/script/c69115073.lua
+++ MDPro3/script/c69115073.lua
@@ -1,30 +1,87 @@
---Rocky Gaucho
+--Rocky Gaúcho
 local s,id=GetID()
 function s.initial_effect(c)
-	--Sincro Genérico
 	c:EnableReviveLimit()
-	aux.AddSynchroProcedure(c,aux.FilterBoolFunction(Card.IsSetCard,0xc50),aux.NonTuner(nil),1)
+	aux.AddSynchroProcedure(c,s.tunerfilter,aux.NonTuner(nil),1,1)
 
-	--Tratar como Nível 6 para Invocações-Sincro
+	-- Synchro Summon effect
 	local e1=Effect.CreateEffect(c)
-	e1:SetType(EFFECT_TYPE_SINGLE)
-	e1:SetProperty(EFFECT_FLAG_SINGLE_RANGE)
-	e1:SetCode(EFFECT_SYNCHRO_LEVEL)
-	e1:SetRange(LOCATION_MZONE+LOCATION_HAND)
-	e1:SetValue(s.slevel)
+	e1:SetCategory(CATEGORY_SPECIAL_SUMMON)
+	e1:SetType(EFFECT_TYPE_SINGLE+EFFECT_TYPE_TRIGGER_O)
+	e1:SetProperty(EFFECT_FLAG_DELAY)
+	e1:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e1:SetCondition(s.spcon)
+	e1:SetTarget(s.sptg)
+	e1:SetOperation(s.spop)
+	e1:SetCountLimit(1,id)
 	c:RegisterEffect(e1)
 end
 
---Função que define o valor do Nível para uso em Invocações-Sincro
-function s.slevel(e,c)
-	--Aqui o jogador escolhe se o monstro será tratado como Nível 5 ou 6
-	Duel.Hint(HINT_SELECTMSG,e:GetHandlerPlayer(),aux.Stringid(id,0)) -- "Escolha o Nível para Invocação-Sincro"
-	local op=Duel.SelectOption(e:GetHandlerPlayer(),aux.Stringid(id,1),aux.Stringid(id,2))
-	--op = 0 → Nível 5 | op = 1 → Nível 6
-	local lv=e:GetHandler():GetLevel()
-	if op==1 then
-		return (6<<16)|lv -- trata como 6 para fins de Sincro
-	else
-		return (5<<16)|lv -- trata como 5 para fins de Sincro
+function s.tunerfilter(c)
+	return c:IsSetCard(0xc50)
+end
+
+function s.spcon(e,tp,eg,ep,ev,re,r,rp)
+	return e:GetHandler():IsSummonType(SUMMON_TYPE_SYNCHRO)
+end
+
+function s.spfilter(c,e,tp,lv)
+	return c:IsSetCard(0xc50)
+		and c:IsLevel(lv)
+		and c:IsCanBeSpecialSummoned(e,0,tp,false,false)
+end
+
+function s.sptg(e,tp,eg,ep,ev,re,r,rp,chk)
+	if chk==0 then
+		if Duel.GetLocationCount(tp,LOCATION_MZONE)<4 then return false end
+		for lv=1,4 do
+			if not Duel.IsExistingMatchingCard(s.spfilter,tp,LOCATION_DECK,0,1,nil,e,tp,lv) then
+				return false
+			end
+		end
+		return true
 	end
+	Duel.SetOperationInfo(0,CATEGORY_SPECIAL_SUMMON,nil,4,tp,LOCATION_DECK)
+end
+
+function s.spop(e,tp,eg,ep,ev,re,r,rp)
+	if Duel.GetLocationCount(tp,LOCATION_MZONE)<4 then return end
+
+	for lv=1,4 do
+		Duel.Hint(HINT_SELECTMSG,tp,HINTMSG_SPSUMMON)
+		local g=Duel.SelectMatchingCard(tp,s.spfilter,tp,LOCATION_DECK,0,1,1,nil,e,tp,lv)
+		if #g>0 then
+			Duel.SpecialSummon(g,0,tp,tp,false,false,POS_FACEUP)
+		end
+	end
+
+	s.applyrestriction(e,tp)
+end
+
+function s.applyrestriction(e,tp)
+	local c=e:GetHandler()
+
+	local e1=Effect.CreateEffect(c)
+	e1:SetType(EFFECT_TYPE_FIELD)
+	e1:SetProperty(EFFECT_FLAG_PLAYER_TARGET)
+	e1:SetCode(EFFECT_CANNOT_SPECIAL_SUMMON)
+	e1:SetTargetRange(1,0)
+	e1:SetTarget(function(e,c)
+		return c:IsLocation(LOCATION_EXTRA) and not c:IsType(TYPE_SYNCHRO)
+	end)
+	Duel.RegisterEffect(e1,tp)
+
+	local e2=Effect.CreateEffect(c)
+	e2:SetType(EFFECT_TYPE_FIELD+EFFECT_TYPE_CONTINUOUS)
+	e2:SetCode(EVENT_SPSUMMON_SUCCESS)
+	e2:SetOperation(function(e,tp,eg,ep,ev,re,r,rp)
+		if eg:IsExists(function(c,tp)
+			return c:IsControler(tp)
+				and c:IsSummonType(SUMMON_TYPE_SYNCHRO)
+		end,1,nil,tp) then
+			e1:Reset()
+			e:Reset()
+		end
+	end)
+	Duel.RegisterEffect(e2,tp)
 end
